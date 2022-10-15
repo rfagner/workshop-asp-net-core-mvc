1434b8700d821d8b45e4ba824208dcda343dd6f5
Fagner Santos <uprogramador@gmail.com>
2022-10-14 21:27:14 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App";
+            ViewData["Desenvolvedor"] = "Renildo Fagner";
 
             return View();
         }
