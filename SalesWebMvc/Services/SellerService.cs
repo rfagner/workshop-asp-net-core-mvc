af5877b8ad20e5f20875209221491d28fe67da95
Fagner Santos <uprogramador@gmail.com>
2022-10-18 16:21:48 -0300

    Simple Create form

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -14,10 +14,22 @@ namespace SalesWebMvc.Services
         {
             _context = context;
         }
-
+        /// <summary>
+        /// Lista de Sellers
+        /// </summary>
+        /// <returns>Lista de Sellers</returns>
         public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
+        }
+        /// <summary>
+        /// Cadastrar um Seller no banco
+        /// </summary>
+        /// <param name="obj">Dados do vendedor</param>
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
         }
     }
 }
